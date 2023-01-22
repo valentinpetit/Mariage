5d19fe0fc5c14187b336d1d5224cbcc1d92c3caf
valentinpetit <master_night_mare@hotmail.fr>
2023-01-22 18:38:11 +0100

    webcam telephone

--- webcam.js
+++ webcam.js
@@ -1,38 +1,68 @@
-let camera_button = document.querySelector("#start-camera");
-let video = document.querySelector("#video");
-let start_button = document.querySelector("#start-record");
-let stop_button = document.querySelector("#stop-record");
-let download_link = document.querySelector("#download-video");
+feather.replace();
 
-let camera_stream = null;
-let media_recorder = null;
-let blobs_recorded = [];
+const controls = document.querySelector('.controls');
+const cameraOptions = document.querySelector('.video-options>select');
+const video = document.querySelector('video');
+const canvas = document.querySelector('canvas');
+const screenshotImage = document.querySelector('img');
+const buttons = [...controls.querySelectorAll('button')];
+let streamStarted = false;
 
-camera_button.addEventListener('click', async function() {
-   	camera_stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-	video.srcObject = camera_stream;
-});
+const [play, pause, screenshot] = buttons;
 
-start_button.addEventListener('click', function() {
-    // set MIME type of recording as video/webm
-    media_recorder = new MediaRecorder(camera_stream, { mimeType: 'video/webm' });
+const constraints = {
+  video: {
+    width: {
+      min: 1280,
+      ideal: 1920,
+      max: 2560,
+    },
+    height: {
+      min: 720,
+      ideal: 1080,
+      max: 1440
+    },
+  }
+};
 
-    // event : new recorded video blob available
-    media_recorder.addEventListener('dataavailable', function(e) {
-		blobs_recorded.push(e.data);
-    });
+const getCameraSelection = async () => {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videoDevices = devices.filter(device => device.kind === 'videoinput');
+  const options = videoDevices.map(videoDevice => {
+    return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
+  });
+  cameraOptions.innerHTML = options.join('');
+};
 
-    // event : recording stopped & all blobs sent
-    media_recorder.addEventListener('stop', function() {
-    	// create local object URL from the recorded video blobs
-    	let video_local = URL.createObjectURL(new Blob(blobs_recorded, { type: 'video/webm' }));
-    	download_link.href = video_local;
-    });
+play.onclick = () => {
+  if (streamStarted) {
+    video.play();
+    play.classList.add('d-none');
+    pause.classList.remove('d-none');
+    return;
+  }
+  if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
+    const updatedConstraints = {
+      ...constraints,
+      deviceId: {
+        exact: cameraOptions.value
+      }
+    };
+    startStream(updatedConstraints);
+  }
+};
 
-    // start recording with each recorded blob having 1 second video
-    media_recorder.start(1000);
-});
+const startStream = async (constraints) => {
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+  handleStream(stream);
+};
 
-stop_button.addEventListener('click', function() {
-	media_recorder.stop();
-});
+const handleStream = (stream) => {
+  video.srcObject = stream;
+  play.classList.add('d-none');
+  pause.classList.remove('d-none');
+  screenshot.classList.remove('d-none');
+  streamStarted = true;
+};
+
+getCameraSelection();
